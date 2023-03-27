97ace07f76eab12afca551f727bb3bc08a2be722
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-27 19:45:01 +0200

    pages ok - now connect them

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Primi Passi</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-success bg-opacity-25">
 
-    <h1>Hello World!</h1>
+    <!-- <h1>Hello World!</h1> -->
+    <nav class="navbar navbar-expand bg-success bg-opacity-50">
+        <div class=" container">
+            <div id="navbarNav">
+                <ul class="navbar-nav">
+                    @foreach($pages as $page)
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> {{ $page }} </a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::get("/", function () {
+    $pages = ["Page 1", "Page 2", "Page 3", "Page 4"];
+    return view("home", compact("pages"));
 });
+
+// Route::get("/pages", function () {
+//     return view("pages.page1");
+// });
